660587f2fdaa2a370839f7045811343505336cc5
Acdaling <acdaling@gmail.com>
2020-10-01 22:31:29 +0200

    Fixed name

--- app/src/main/java/com/example/madlevel4task1/ProductDao.kt
+++ app/src/main/java/com/example/madlevel4task1/ProductDao.kt
@@ -7,7 +7,7 @@ import androidx.room.Query
 
 @Dao
 interface ProductDao {
-    @Query("SELECT * FROM product_table")
+    @Query("SELECT * FROM productTable")
     suspend fun getAllProducts(): List<Product>
 
     @Insert
@@ -16,7 +16,7 @@ interface ProductDao {
     @Delete
     suspend fun deleteProduct(product: Product)
 
-    @Query("DELETE FROM product_table")
+    @Query("DELETE FROM productTable")
     suspend fun deleteAllProducts()
 
 }
