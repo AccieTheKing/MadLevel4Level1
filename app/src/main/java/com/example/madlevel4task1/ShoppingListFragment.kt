fd52eba60502b9722a50c7732471d3a264c69bbd
Acdaling <acdaling@gmail.com>
2020-10-02 15:23:31 +0200

    Added the repository to the activity

--- app/src/main/java/com/example/madlevel4task1/ShoppingListFragment.kt
+++ app/src/main/java/com/example/madlevel4task1/ShoppingListFragment.kt
@@ -1,31 +1,41 @@
 package com.example.madlevel4task1
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class ShoppingListFragment : Fragment() {
+    // the lateinit var tells kotlin that it's going to be initialized at a later point
+    private lateinit var productRepository: ProductRepository
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shoppingList, container, false)
+        return inflater.inflate(R.layout.fragment_shoppinglist, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        productRepository = ProductRepository(requireContext())
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        initView()
+    }
+
+
+    /**
+     * This method will initialize all the listeners and everything that needs to be done after the
+     * view is created
+     */
+    private fun initView(){
+
     }
 }
